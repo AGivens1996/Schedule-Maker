c737c8b83cfb30234b23d4ee91777835437939d8
AGivens1996 <agivens1996@gmail.com>
2016-09-17 10:44:48 -0500

    Create scheduleMaker.h

---
+++ scheduleMaker.h
@@ -0,0 +1,12 @@
+#ifndef SCHEDULEMAKER_H
+#define SCHEDULEMAKER_H
+#include <iostream>
+#include <ctime>
+#include <cstring>
+#include <fstream>
+using namespace std;
+
+//Puts input in a text file
+void inputFile(&char teamName[], 
+
+#endif
